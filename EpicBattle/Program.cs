9316e73b52a779c1bbc01432b5bc8d58eafd430a
Andrew Malkov <anmalkov@microsoft.com>
2021-03-05 02:28:13 +0100

    Episode 2

---
+++ EpicBattle/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace EpicBattle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("\t                     -----------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\t---------------------------------------------------------------------------------------");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("\t       *****  ****    ***    ***          ****    ****  *****  *****  *      *****");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("\t       *      *   *    *    *   *         *   *  *   *    *      *    *      *");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("\t       ***    ****     *    *             ****   *****    *      *    *      ***");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\t       *      *        *    *   *         *   *  *   *    *      *    *      *");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\t       *****  *       ***    ***          ****   *   *    *      *    *****  *****");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\t---------------------------------------------------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("\t                     -----------------------------------------------");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("\t                                                           v1.0");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("  2021 (C) Andrew Malkov");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ResetColor();
+        }
+    }
+}
+
+/*
+
+                     -----------------------------------------------
+---------------------------------------------------------------------------------------
+
+       *****  ****    ***    ***          ****    ****  *****  *****  *      *****
+       *      *   *    *    *   *         *   *  *   *    *      *    *      *
+       ***    ****     *    *             ****   *****    *      *    *      ***
+       *      *        *    *   *         *   *  *   *    *      *    *      *
+       *****  *       ***    ***          ****   *   *    *      *    *****  *****
+
+---------------------------------------------------------------------------------------
+                     -----------------------------------------------
+
+                                                           v1.0  2021 (C) Andrew Malkov
+ */
